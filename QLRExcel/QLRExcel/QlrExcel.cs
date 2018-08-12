284e4c20daebdca5f9dd0df74e9aca9cf81c4cf9
Anders Engan <anders-eng@hotmail.com>
2018-08-12 11:41:58 +0200

    Added NuGet package ExcelDna.Addin to QLRExcel

--- QLRExcel/QLRExcel/QlrExcel.cs
+++ QLRExcel/QLRExcel/QlrExcel.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using QLRAnalytics;
+using QLRData;
+using ExcelDna.Integration;
 
 namespace QLRExcel
 {
     public class QlrExcel
     {
+        private enum ExcelTaskStatus
+        {
+            IN_PROGRESS,
+            COMPLETED
+        }
+        
+        [ExcelFunction(Category = "QLNetRisk Excel", Description = "Run app")]
+        public static object App()
+        {
+            string message = ExcelTaskStatus.IN_PROGRESS.ToString();
+
+            try
+            {                
+                Parameters parameters = new Parameters();
+                //parameters.FromFile("C:\\developer\\src\\cpp\\Engine\\Examples\\Example_FXFWD\\Input\\ore.xml");
+                parameters.FromFile("C:\\developer\\git\\Engine\\Examples\\Example_FXFWD\\Input\\ore.xml");
+                Directory.SetCurrentDirectory("C:\\developer\\git\\Engine\\Examples\\Example_FXFWD");
+
+                App app = new App(parameters);
+                app.Run();
+
+                message = ExcelTaskStatus.COMPLETED.ToString();
+            }
+            catch (Exception ex)
+            {
+                message = ExcelError.ExcelErrorNA.ToString();
+            }
+
+            return message;
+        }
     }
 }
